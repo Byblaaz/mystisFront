fb408c1b2e715ec2a62e3cd420ea337b33de2382
Ben <benbugarindev@gmail.com>
2022-11-05 20:59:09 +0100

    database + scene tutorial setup

---
+++ src/config/firebase-config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBm-2nlPnvbZ1Vx-kzucDzwO7404rDdQug",
+    authDomain: "mystis-58531.firebaseapp.com",
+    projectId: "mystis-58531",
+    storageBucket: "mystis-58531.appspot.com",
+    messagingSenderId: "970146247741",
+    appId: "1:970146247741:web:16c6b66b55c2f543e4e654"
+  };
+  export const app = initializeApp(firebaseConfig);
--- src/plugins/PlayerState.js
+++ src/plugins/PlayerState.js
@@ -3,6 +3,11 @@ import { ethers } from "ethers";
 import { connect } from "@argent/get-starknet"
 import {Contract} from 'starknet';
 
+// Firebase imports
+import { getFirestore, collection, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+import { app } from "../config/firebase-config"
+
+// Smart-Contract variables
 import mystisabi from "../abi/mystisabi.json"
 const MystisAddress = "0x018743ab8fd75ed0fcfe5581aca191bc166f0997cb9851710679adf8972faa35"
 
@@ -10,14 +15,24 @@ class Player extends Phaser.Plugins.BasePlugin {
     constructor(pluginManager) {
         super(pluginManager);
 
+        this.db = getFirestore();
 
+        this.users = collection(this.db, 'users');
+        console.log(this.users);
         //Global States
         this.playerInfo = {
             name: '',
             address: null,
-            account: ''
+            lastLogin: new Date(),
+            isFirstTime: true,
+            dateJoined: null,
         }
+        this.playerBlockchainData = {
+            account: '',
+        }
+ 
     }
+
     async loadWeb3() {
             await this.loadBlockchainData();
     }
@@ -41,29 +56,49 @@ class Player extends Phaser.Plugins.BasePlugin {
             await windowStarknet?.enable({ starknetVersion: "v4" })
             if (windowStarknet.selectedAddress) {
                 if (windowStarknet.chainId === "SN_GOERLI") {
+                    //Getting data from /users/${account[0]}
+                    const userRef = doc(this.db, "users", windowStarknet.selectedAddress);
+                    console.log(userRef)
+                    const user = await getDoc(userRef);
+                    console.log(user)
+                    
+                    if (user.exists()) {
+                        const { isFirstTime, name,lastLogin, dateJoined, } = user.data();
+                        console.log("address starknet "+windowStarknet.selectedAddress)
+                        await this.getNft(windowStarknet.selectedAddress);
+                        this.playerBlockchainData.account = windowStarknet.account
+                        this.setPlayerInfo(name, windowStarknet.selectedAddress, isFirstTime, lastLogin.toDate(), dateJoined.toDate());
 
-                    console.log( windowStarknet.account)
-                    this.playerInfo.account = windowStarknet.account
-                    this.playerInfo.address = windowStarknet.selectedAddress
-                    console.log("address starknet "+windowStarknet.selectedAddress)
-                    await this.getNft(windowStarknet.selectedAddress);
-
+                    }else{
+                        this.playerInfo.address = windowStarknet.selectedAddress
+                        this.playerBlockchainData.account = windowStarknet.account
+                        this.playerInfo.isFirstTime = true
+                        
+                    }
                 } else {
                     alert("Invalid network, switch to Goerli and try again");
 
                 }
             }
         } catch (e) {
-            console.log(e.message);
+            console.log(e);
             window.alert('Unable to connect to your Argent X wallet. Please try again later');
         }
+    }
+    setPlayerInfo(name, address,isFirstTime, account,lastLogin){
+        this.playerInfo = {
+            name,
+            address,
+            lastLogin,
+            isFirstTime,
+        }
+        console.log('Player Info Set :',this.playerInfo);
     }
 
     getNft = async (address) => {
 
         let blockchainNFT = [];
-        const contract = new Contract(mystisabi, MystisAddress, this.playerInfo.account);
-        console.log(contract)
+        const contract = new Contract(mystisabi, MystisAddress, this.playerBlockchainData.account);
         try {
 
             let nbNFTMint = await contract.balanceOf(address);

ea44642038cf1d82725d09c9ee14e91914686056
Louis <dussoulier.louis@hotmai.fr>
2022-11-06 13:24:34 +0100

    Test animation brigand

--- src/components/Game.js
+++ src/components/Game.js
@@ -39,15 +39,17 @@ function Game() {
     });
 
     function preload(){
-        console.log(window)
         this.load.setPath('assets/brigand/');
+        this.load.image('homeButton', 'assets/home.png');
+
         this.load.spine('player', 'brigand.json', 'brigand.atlas', true);
 
     }
 
     function create(){
 
-        console.log("erreur 3")
+        this.buttonHome = this.add.sprite(this.cameras.main.width / 50 , this.cameras.main.height / 20, 'homeButton').setScale(0.3).setInteractive();
+
         //var boy = this.add.spine(600, 600, 'player', 'idle', true).ToScale(0.5);
 
         let boy1 = this.make.spine({
@@ -55,11 +57,46 @@ function Game() {
             scale: 0.5,
             skinName: 'default',
             animationName: 'idle', loop: true,
-            slotName: 'tete', attachmentName: 'tete'
+            slotName: 'tete', attachmentName: 'tete_capuche',
+            // tete {tete_1,tete2,}
         });
-        console.log(boy1.getBounds());
+
+        const attachementsPantalon = ['jambe droite_', 'jambe gauche_', 'mollet droit_', 'mollet gauche_']
 
 
+
+        const getAttachments = (spine) => {
+            return spine.skeleton.skin.attachments
+        }
+        const setAttachment = (spine, slotName, attachmentName) => {
+            spine.skeleton.setAttachment(slotName, attachmentName)
+        }
+
+        console.log(getAttachments(boy1));
+
+        let listeChangeAttachement = [];
+        console.log(listeChangeAttachement.length)
+        if (listeChangeAttachement.length == 0) {
+            const trait = "pantalon_blue"
+            var attribut = trait.split("_")[1]
+            console.log(attribut)
+            for (const item in attachementsPantalon) {
+                console.log(item)
+                listeChangeAttachement.push(String(attachementsPantalon[item]+attribut))
+                console.log(attachementsPantalon[item].slice(0, -1))
+                setAttachment(boy1, String(attachementsPantalon[item].slice(0, -1)), String(attachementsPantalon[item]+attribut))
+            }
+            console.log(listeChangeAttachement)
+            setAttachment(boy1, "tete", "tete")
+            //setAttachment(boy1, "tete", "tete")
+
+            //this.sound.play('hoverEffect', { loop: false });
+        }
+
+        this.buttonHome.on('pointerover', () => {
+
+
+        });
 
     }
 
